a9ef8e0d27e347dffc738bac13d3ba59a5da5c24
Bishal11122 <bishal5627x@gmail.com>
2026-02-22 08:03:36 +0000

    container config done

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -3,17 +3,31 @@
 Primitive-Number,Strings,Boolen,Null,Undefined.
 Reference-Array,Objects,Functions.*/
 
-//Strings- "" - double quotes,'' - single quotes, `` - Backticks.
+//Strings-"" - double quotes,'' - single quotes, `` - Backticks.
 
 let firstName="Bishal";
 let lastName="Sharma";
 let age=20;
 console.log(firstName+""+lastName);
-console.log(`${firstName} ${lastName}`)
+console.log(`${firstName} ${lastName}`);
 console.log(`My name is ${firstName} and I am ${age} years old.`);
 
-//Numbers- it can stores any type like whole number,Decimal number,Negative Number.
+// Numbers- it can stores any type like whole number,Decimal number,Negative Number.
 
-let newAge=20; // whole number
-let marks=33.35; // decimal point number 
-let temp=-4; // Negative Number 
+let newAge= 20; // whole number
+let marks= 33.35; // decimal point number 
+let temp= -4; // Negative Number 
+
+console.log(newAge);
+console.log(marks);
+console.log(temp);
+
+//Boolen-True and False 
+
+//Null- 
+let marks_eng = null;
+console.log(marks_eng);
+
+//Undefined- 
+let marks_maths;
+console.log(marks_maths);
