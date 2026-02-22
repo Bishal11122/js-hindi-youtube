fa33ce446da945453aca760ee0f8808b4bf05094
Bishal11122 <bishal5627x@gmail.com>
2026-02-22 07:48:14 +0000

    container config done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,19 @@
+/*Data type are classified into two types:Primitive and Non-Primitive(Reference)Data Type.
+
+Primitive-Number,Strings,Boolen,Null,Undefined.
+Reference-Array,Objects,Functions.*/
+
+//Strings- "" - double quotes,'' - single quotes, `` - Backticks.
+
+let firstName="Bishal";
+let lastName="Sharma";
+let age=20;
+console.log(firstName+""+lastName);
+console.log(`${firstName} ${lastName}`)
+console.log(`My name is ${firstName} and I am ${age} years old.`);
+
+//Numbers- it can stores any type like whole number,Decimal number,Negative Number.
+
+let newAge=20; // whole number
+let marks=33.35; // decimal point number 
+let temp=-4; // Negative Number 
